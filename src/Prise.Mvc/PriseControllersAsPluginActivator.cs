a46e1e8434a63d37201c26268c71b6a51ca479ff
Maarten MERKEN <maarten.merken@aldautomotive.com>
2020-01-14 07:30:18 +0100

    Cleanup

--- src/Prise.Mvc/PriseControllersAsPluginActivator.cs
+++ src/Prise.Mvc/PriseControllersAsPluginActivator.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
-using Prise;
 using Prise.Infrastructure;
 using Prise.Plugin;
 
